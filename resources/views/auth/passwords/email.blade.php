f2aa9cb0533d96798e6faa1264aa8d5883f77e6d
Ildemar <ildemar.92@gmail.com>
2018-05-19 14:45:40 -0400

    Landing template integration

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,65 @@
-@extends('layouts.app')
+@extends('layouts.landing')
+
+@section('title')
+    SPK Venezuela | Recuperar Contraseña
+@endsection
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <nav class="navbar navbar-ct-transparent navbar-fixed-top" role="navigation-demo" id="register-navbar">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="/">SPK Venezuela</a>
+            </div>
 
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse" id="navigation-example-2">
+                <ul class="nav navbar-nav navbar-right">
+                    <li>
+                        <a href="{{ route('login') }}" class="btn btn-simple">Ingresar</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('register') }}" class="btn btn-simple">Registrar</a>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </div><!-- /.container-->
+    </nav>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+    <div class="wrapper">
+        <div class="register-background" style="background-image: url({{ asset('img/paper_img/landscape.jpg') }});">
+            <div class="filter-black"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1 ">
+                        <div class="register-card">
+                            <form class="register-form" method="POST" action="{{ route('password.email') }}">
+                                {{ csrf_field() }}
 
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
                                 @endif
-                            </div>
-                        </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
+                                <div class="form-group {{ $errors->has('email') ? 'has-error' : '' }}">
+                                    <label class="text-primary">Email</label>
+                                    <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+                                </div>
+
+                                <button type="submit" class="btn btn-danger btn-block">Send Password Reset Link</button>
+                            </form>
                         </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
+
---
+++ resources/views/layouts/landing.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0' name='viewport' />
+    <meta name="viewport" content="width=device-width" />
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>@yield('title')</title>
+
+    <link rel="icon" href="{{ asset('img/paper_img/favicon.ico') }}">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/lib/ct-paper.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/lib/ct-dm.css') }}" rel="stylesheet"/>
+    <link href="{{ asset('css/lib/ct-xpl.css') }}" rel="stylesheet"/>
+
+    {{--Fonts and icons--}}
+    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+
+</head>
+<body>
+    {{--content--}}
+    @yield('content')
+    {{--/content--}}
+
+    <script src="{{ asset('js/jquery.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+
+    {{--Plugins--}}
+    <script src="{{ asset('js/lib/ct-paper-checkbox.js') }}"></script>
+    <script src="{{ asset('js/lib/ct-paper-radio.js') }}"></script>
+    {{--<script src="{{ asset('js/bootstrap-select.js') }}"></script>--}}
+    {{--<script src="{{ asset('js/bootstrap-datepicker.js') }}"></script>--}}
+    <script src="{{ asset('js/lib/ct-paper.js') }}"></script>
+</body>
+</html>
